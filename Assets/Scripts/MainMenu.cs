bc5f497f36eea322ae2976716ce2a0a26cea63c6
dev_chow <56135351+devchow@users.noreply.github.com>
2021-12-01 10:41:35 +0300

    menu update 2

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,18 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Play Button - Loads the Game Scene
+    public void StartGame()
     {
-        
+        SceneManager.LoadScene("Game");
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    // Close Game
+    public void QuitGame()
     {
-        
+        Application.Quit();
+        Debug.Log("Game Closed!!");
     }
 }
